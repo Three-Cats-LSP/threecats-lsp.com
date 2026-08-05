18c8bc9fafcc56f6f598f1902a89763bf7ec0e61
Roman Markovtsev <sns@rvm.cc>
2026-08-05 19:01:03 +0900

    Deploy telemetry popup guidance

--- seabirds/sw.js
+++ seabirds/sw.js
@@ -1,4 +1,4 @@
-const CACHE='seabirds-v11';
+const CACHE='seabirds-v12';
 const ASSETS=['./','index.html','app.css','seabirds.css','app.js','shearwater.js','sync.js','plot-core.js','manifest.webmanifest','icon.svg','firebase-config.js','vendor/firebase/firebase-app-compat.js','vendor/firebase/firebase-auth-compat.js','vendor/firebase/firebase-firestore-compat.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
