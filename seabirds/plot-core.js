312ca55a6055c324fb0661e9398b7454dd1b6107
Roman Markovtsev <sns@rvm.cc>
2026-08-05 21:11:09 +0900

    Rename average depth label

--- seabirds/plot-core.js
+++ seabirds/plot-core.js
@@ -880,7 +880,7 @@ function attachDiveProfileInteraction(canvasId) {
     }
     if (telemetry?.meanDepth != null) {
       const meanDepth = units === 'imperial' ? telemetry.meanDepth * 3.28084 : telemetry.meanDepth;
-      html += `<div>Mean depth ${meanDepth.toFixed(1)} ${du}</div>`;
+      html += `<div>Avr. Depth ${meanDepth.toFixed(1)} ${du}</div>`;
     }
     if (telemetry?.ndl != null) html += `<div>NDL ${telemetry.ndl} min</div>`;
     if (telemetry?.tts != null) html += `<div>TTS ${telemetry.tts} min</div>`;
