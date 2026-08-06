f0b778e90282341b5b2ab749c88e00fa77e4e78b
Roman Markovtsev <sns@rvm.cc>
2026-08-06 20:53:34 +0900

    Publish SeaBirds 0.1.11 device filters

--- seabirds/app-version.js
+++ seabirds/app-version.js
@@ -1,1 +1,1 @@
-(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.10'})(typeof window!=='undefined'?window:self);
+(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.11'})(typeof window!=='undefined'?window:self);
