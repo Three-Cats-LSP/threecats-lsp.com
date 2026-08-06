4c71b0b418e7b6667edbadebd264e1840d77c142
Roman Markovtsev <sns@rvm.cc>
2026-08-06 15:32:34 +0900

    Defer SeaBirds equipment sync

--- seabirds/sw.js
+++ seabirds/sw.js
@@ -1,4 +1,4 @@
-const CACHE='seabirds-v59';
+const CACHE='seabirds-v60';
 const ASSETS=['./','index.html','app.css','seabirds.css','app.js','shearwater.js','sync.js','plot-core.js','manifest.webmanifest','icon.svg','shearwater-logo.png','tab-profile.png','tab-notes.png','tab-equipment.png','tab-information.png','app-threecats.png','app-dplanner.png','app-tviewer.png','app-getinwater.png','firebase-config.js','vendor/firebase/firebase-app-compat.js','vendor/firebase/firebase-auth-compat.js','vendor/firebase/firebase-firestore-compat.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
