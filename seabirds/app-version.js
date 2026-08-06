44b5a0921989f7f3014d19ae210c774e9adc34dc
Roman Markovtsev <sns@rvm.cc>
2026-08-06 17:50:10 +0900

    Publish SeaBirds 0.1.5 recovery release

--- seabirds/app-version.js
+++ seabirds/app-version.js
@@ -1,1 +1,1 @@
-(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.4'})(typeof window!=='undefined'?window:self);
+(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.5'})(typeof window!=='undefined'?window:self);
