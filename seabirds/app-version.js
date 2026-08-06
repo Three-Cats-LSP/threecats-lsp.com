fb5707802c12ddaf21802f0e1fe1e5f3637f785f
Roman Markovtsev <sns@rvm.cc>
2026-08-06 18:38:38 +0900

    Publish SeaBirds 0.1.6 native release

--- seabirds/app-version.js
+++ seabirds/app-version.js
@@ -1,1 +1,1 @@
-(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.5'})(typeof window!=='undefined'?window:self);
+(function(root){'use strict';root.SEABIRDS_APP_VERSION='0.1.6'})(typeof window!=='undefined'?window:self);
